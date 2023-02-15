f1eda6720eda4990c85e3d7456903646777b4f9b
Dany Dubé <74633244+danydube1971@users.noreply.github.com>
2023-02-15 06:45:55 -0500

    Add files via upload

---
+++ Joindre des PDF.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+"""Ce script utilise la bibliothèque PyMuPDF pour fusionner les fichiers PDF dans le répertoire courant. Les fichiers PDF sont triés par ordre alphabétique avant d'être fusionnés. Il faut installer le module PyMuPDF avec la commande pip3 install PyMuPDF"""
+
+import os
+import fitz
+
+# Récupérer tous les fichiers PDF dans le répertoire courant
+pdf_files = [f for f in os.listdir() if f.endswith('.pdf')]
+
+# Trier les fichiers PDF par ordre alphabétique
+pdf_files.sort()
+
+# Créer un objet PdfFileMerger pour fusionner les fichiers PDF
+pdf_merger = fitz.open()
+
+# Ajouter chaque fichier PDF à l'objet PdfFileMerger
+for filename in pdf_files:
+    with fitz.open(filename) as pdf_file:
+        pdf_merger.insert_pdf(pdf_file)
+
+# Écrire le fichier PDF final avec un nom se terminant par .join
+pdf_merger.save("output.join.pdf")
+
+print("Fusion des fichiers PDF terminée.")
+
